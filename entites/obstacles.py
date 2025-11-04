f290c328ca13c8e55052e1c8a93b6241952c0c3f
Lucas <morvan.lucas50@gamil.com>
2025-11-04 14:14:03 +0900

    obstacle

--- entites/obstacles.py
+++ entites/obstacles.py
@@ -1,1 +1,15 @@
 
+class SphericalObstacle:
+    def __init__(self, center, radius, obstacle_id=1):
+        self.center = center  # in meter
+        self.radius = radius  # in meter
+        self.id = obstacle_id
+
+
+class CubeObstacle:
+    def __init__(self, center, length, width, height, obstacle_id=2):
+        self.center = center
+        self.length = length
+        self.width = width
+        self.height = height
+        self.id = obstacle_id
