efda573e933c291e4313b138e78ce88b3d05d1b5
adrian-ujkaj <a22.ujkaj@gmail.com>
2025-11-17 16:55:10 +0900

    sensor

--- entites/sensor.py
+++ entites/sensor.py
@@ -1,37 +1,43 @@
 import numpy as np
 
 class Sensor:
+    """Classe de base pour les capteurs (identique à votre PDF )."""
     def __init__(self):
         pass
-
+        
     def measure(self, *args, **kwargs):
-        raise NotImplementedError("The 'measure' method must be implemented by the subclass")
+        raise NotImplementedError("La méthode 'measure' doit être implémentée par la sous-classe")
 
-
-class GNSS_Sensor(Sensor):
-    def __init__(self, 
-                 position_noise_std: float , 
-                 velocity_noise_std: float  
-                ):
-        
+class GPSSensor(Sensor):
+    """
+    Capteur GPS qui lit ses paramètres depuis un objet config.
+    """
+    def __init__(self, config):
+        """
+        Initialise le capteur GPS.
+        'config' est un dictionnaire, par ex:
+        {'position_noise_std': 0.05, 'velocity_noise_std': 0.02}
+        """
         super().__init__()
-        self.pos_noise_std = float(position_noise_std)
-        self.vel_noise_std = float(velocity_noise_std)
         
+        # Lit les paramètres depuis l'objet config
+        self.pos_noise_std = float(config.get('position_noise_std', 0.0))
+        self.vel_noise_std = float(config.get('velocity_noise_std', 0.0))
+
         if self.pos_noise_std < 0:
             self.pos_noise_std = 0
         if self.vel_noise_std < 0:
             self.vel_noise_std = 0
-
-    def measure(self, 
-                ground_truth_position: np.ndarray, 
+            
+    def measure(self,
+                ground_truth_position: np.ndarray,
                 ground_truth_velocity: np.ndarray
-               ) -> tuple[np.ndarray, np.ndarray]:
+                ) -> tuple[np.ndarray, np.ndarray]:
         
+        # (Code identique à votre PDF )
         pos_noise = np.random.normal(0.0, self.pos_noise_std, 3)
-        
         vel_noise = np.random.normal(0.0, self.vel_noise_std, 3)
-
+        
         measured_position = ground_truth_position + pos_noise
         measured_velocity = ground_truth_velocity + vel_noise
         
